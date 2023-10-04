d8a5c7a4649e7d2a33c1f097d12ef3bcc297d0c7
Carlo Tacchella <tacchella.carlo@gmail.com>
2023-10-04 21:08:00 +0200

    SEO in React

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,7 +1,15 @@
 import {Link} from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+
 function About(){
     return( 
         <>
+            <Helmet>
+                <title>About</title>
+                <meta name="description" content="About myself"/>
+                <link rel = "canonical" href = "/about" />
+                <meta name="robots" content="noindex"/>
+            </Helmet>
             <h1>My About Page</h1>
             <p>Go to <Link to="/">home</Link></p>
         </>
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,9 +1,15 @@
 import {Link} from 'react-router-dom';
-
+import { Helmet } from 'react-helmet-async';
 function Home(){
    return( 
        <div>
-         <h1>My Home Page</h1>
+        <Helmet>
+            <title>Home</title>
+            <meta name="description" content="My Homepage"/>
+            <link rel = "canonical" href = "/" />
+            <meta name="robots" content="noindex"/>
+        </Helmet>
+         <h1>My Home Page v. 0.0.1</h1>
          <p>Go to <Link to="/about">about</Link></p>
        </div>
      ); 
